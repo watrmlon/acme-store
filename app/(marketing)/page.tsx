cdc9020782d5d98199c65244dd87e72dde8030e1
watermelonbit <enoabasidream@gmail.com>
2024-02-11 15:23:07 +0400

    build: add supabase & prisma - Added Prisma ORM - Added Supbase database - Added Shadcn/ui components

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="bg-muted px-5 py-1.5 rounded-lg">
+        <h1 className="font-medium text-lg">Welcome to PeakBuy</h1>
+      </div>
+    </main>
+  );
+}
